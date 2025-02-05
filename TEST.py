e5e6a083b044e73cd108f9faf90faa1a98ab04fb
inventure71 <matteo.giorgetti.05@gmail.com>
2025-02-05 22:23:16 +0100

    First batch

--- TEST.py
+++ TEST.py
@@ -1,65 +1,19 @@
-#from utils.circle_detection_utils import divide_picture_into_cells
-import sys
-import time
+import requests
 
-#from connect4 import predict_move
-#from utils.connect4_utils import find_possible_position_of_next_move
-#from utils.useTeachableMachine import CircleRecognition
-#import tensorflow as tf
-
-#divide_picture_into_cells("Images/rectified_image.jpg", 7, 6)
-
-import h5py
-f = h5py.File("Models/teachable/keras_model.h5", mode="r+")
-model_config_string = f.attrs.get("model_config")
-if model_config_string.find('"groups": 1,') != -1:
-    model_config_string = model_config_string.replace('"groups": 1,', '')
-    f.attrs.modify('model_config', model_config_string)
-    f.flush()
-    model_config_string = f.attrs.get("model_config")
-    assert model_config_string.find('"groups": 1,') == -1
-
-f.close()
-
-'''
-matrix, image = process_each_cell("Images/rectified_image.jpg", 7, 6)
-for row in matrix:
-    print(row)
-
-matrix = np.matrix(([0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 1, 0, 1, 2, 0]))
-
-last_matrix = np.matrix(([0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 1, 2, 0]))
-
-new_matrix = matrix-last_matrix
-
-print(new_matrix.mean()*6*7)
-print(new_matrix.max())
-print(new_matrix)
+SERVER_URL = "http://192.168.206.132:5000/move"  # Replace with your local IP
 
 
-while True:
-    fps =+ 1
-    status_message = "Waiting for input"
+def send_move_request(column):
+    response = requests.post(SERVER_URL, json={"column": column})
+    print(response.json())
 
-    import sys
-
-    # Replace this section in your loop
-    print(f"\rFPS: {fps:.2f} | Matrix:\n{matrix} | Status: {status_message}", end='', flush=True)
-
-    time.sleep(1)
-
-
-matrix = numpy.array(matrix)
-
-model = tf.keras.models.load_model("connect4_model_v1.h5")
-print(predict_move(model, matrix))'''
+if __name__ == "__main__":
+    while True:
+        col = input("Enter column (0-6) or 'q' to quit: ").strip()
+        if col.lower() == 'q':
+            break
+        try:
+            col = int(col)
+            send_move_request(col)
+        except ValueError:
+            print("Invalid input. Please enter a number between 0 and 6.")
--- utils/FIX-Teachable-Machine.py
+++ utils/FIX-Teachable-Machine.py
@@ -1,6 +1,6 @@
 
 import h5py
-f = h5py.File("Models/teachable/keras_model.h5", mode="r+")
+f = h5py.File("../Models/teachable/keras_model.h5", mode="r+")
 model_config_string = f.attrs.get("model_config")
 if model_config_string.find('"groups": 1,') != -1:
     model_config_string = model_config_string.replace('"groups": 1,', '')
